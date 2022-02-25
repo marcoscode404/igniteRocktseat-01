c9b545e72ce7e3a41900b1f0b198156198e16bb7
Marcos Vini/trabalho <viniciusmg99@outlook.com>
2022-02-24 22:41:56 -0400

    Iniciando o curso de React no ignite
    Aula 01 do curso ignite / construindo projeto API do github

---
+++ cursoReact/github-aulas/minha-primeira-app/src/components/RepositoryList.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import { RepositoryItem } from './RepositoryItem';
+
+import '../styles/repositories.scss';
+
+
+// https://api.github.com/orgs/rocktseat/repos
+
+
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+    
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    },[]);
+    // executa uma função quando a variavel mudar
+
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositorio</h1>
+
+            <ul>
+                {repositories.map(repository => (
+                      <RepositoryItem key={repository.name} repository={repository} />
+                    
+                ))}
+              
+             
+                     
+                    
+            </ul>
+        </section>
+    )
+}
